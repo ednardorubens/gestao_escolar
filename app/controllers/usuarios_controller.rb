7afc460c2c46c0b400908760105358f442cb0869
Ednardo Rubens <ednardorubens@hotmail.com>
2019-10-11 16:22:43 -0300

    Update usuarios_controller.rb

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -3,7 +3,7 @@ class UsuariosController < ApplicationController
 
   # GET /usuarios
   def index
-    @usuarios = Usuario.all
+    @usuarios = Usuario.order(:nome, email: :desc)
   end
 
   # GET /usuarios/1
